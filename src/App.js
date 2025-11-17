adf805ed59c412d23c98b0786381b619cf14483c
Marvelous Iretomiwa <Marveltoms2014@gmail.com>
2025-11-17 19:35:25 +0100

    Initial commit - Next.js project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from "./components/Main.jsx"
+
+function App() {
+  return (
+
+
+ <Main/>
+
+    
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from '../asset/247techspywarelandscape.png'
+
+
+const Main = () => {
+
+
+  return (
+    <div className='App'>
+      <div>
+        <img src={Logo} alt='logo' />
+      </div>
+
+     
+      <h1>We will be right back!</h1>
+    </div>
+  )
+}
+
+
+export default Main
